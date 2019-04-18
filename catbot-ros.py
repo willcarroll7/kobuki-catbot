0df2bc96f1fb710caa1692b2863e384399d93c98
Will Carroll <will.carroll7@gmail.com>
2019-04-18 18:57:00 -0500

    finished moving application code into catbot-ros.py implemented prototype 180 degree turn code

--- catbot-ros.py
+++ catbot-ros.py
@@ -11,17 +11,24 @@ import rospy
 import kobuki_msgs.msg as kmm
 import geometry_msgs.msg as gmm
 
-# Constants
+# FSM States
 STATE_SCAN_OBJECTS = 0
-STATE_DIVERT_EDGE = 99
+STATE_DIVERT_EDGE = 98
+# TODO: ...
+STATE_TURN_180 = 99
 
+# Constants
 CLIFF_SENSORS = ["Left", "Center", "Right"]
+NAV_TURN_SPEED = math.pi / 4.0
+ENC_CLICKS_PER_DEG = 24.444444444
+ENC_CLICKS_SLACK = ENC_CLICKS_PER_DEG * 3.0  # 3.0 degrees of slack
 
 # Global Variables
 robot_state = STATE_SCAN_OBJECTS
+left_encoder = right_encoder = 0
 
 
-def cliff_event_callback(event):
+def cliff_event_callback(event: kmm.CliffEvent):
     """Triggered upon Turtlebot cliff detector change event.
 
     If an edge is detected, the robot enters a special edge diversion state which redirects the robot away from the edge
@@ -40,3 +47,48 @@ def cliff_event_callback(event):
 
     robot_state = STATE_DIVERT_EDGE
 
+
+def sensor_core_callback(msg: kmm.SensorState):
+    """Triggered upon Turtlebot sensor data received."""
+
+    global left_encoder, right_encoder
+
+    left_encoder = msg.left_encoder
+    right_encoder = msg.right_encoder
+
+
+# Setup application and run FSM loop
+if __name__ == "__main__":
+    rospy.init_node("catbot", anonymous=True)
+    rospy.Subscriber("/mobile_base/events/cliff", kmm.CliffEvent, cliff_event_callback)
+    rospy.Subscriber("/mobile_base/sensors/core", kmm.SensorState, sensor_core_callback)
+    navi = rospy.Publisher("/cmd_vel_mux/input/navi", gmm.Twist, queue_size=10)
+    rate = rospy.rate(10)  # 10 Hz
+
+    # State helper variables
+    left_encoder_target = right_encoder_target = 0
+    while not rospy.is_shutdown():
+        if robot_state == STATE_SCAN_OBJECTS:
+            continue  # TODO
+
+        elif robot_state == STATE_DIVERT_EDGE:
+            # Set the encoder targets for a 180 degree turn
+            left_encoder_target = left_encoder - (ENC_CLICKS_PER_DEG * 180.0)
+            right_encoder_target = right_encoder + (ENC_CLICKS_PER_DEG * 180.0)
+            robot_state = STATE_TURN_180
+            continue
+
+        elif robot_state == STATE_TURN_180:
+            # Return to STATE_SCAN_OBJECTS if we've finished turning around
+            if abs(left_encoder - left_encoder_target) < ENC_CLICKS_SLACK or abs(
+                    right_encoder - right_encoder_target) < ENC_CLICKS_SLACK:
+                robot_state = STATE_SCAN_OBJECTS
+
+            # Else keep turning
+            else:
+                twist = gmm.Twist()
+                twist.angular.z = NAV_TURN_SPEED
+                navi.publish(twist)
+            continue
+
+        rate.sleep()
