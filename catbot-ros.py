4e726ac52e77a9d48a879d7f3c3e49324371a989
jecossitt <jecossitt@gmail.com>
2019-04-19 18:59:41 -0500

    Added yeetability

--- catbot-ros.py
+++ catbot-ros.py
@@ -13,14 +13,17 @@ import geometry_msgs.msg as gmm
 
 # FSM States
 STATE_SCAN_OBJECTS = 0
+STATE_PREPARE_YEET = 95
+STATE_YEET = 96
 STATE_ALIGN_OBJECT = 97
 STATE_DIVERT_EDGE = 98
-# TODO: ...
 STATE_TURN_180 = 99
 
 # Constants
 CLIFF_SENSORS = ["Left", "Center", "Right"]
 NAV_TURN_SPEED = math.pi / 4.0
+YEET_SPEED = math.pi
+FORWARD_SPEED = 1.0
 ENC_CLICKS_PER_DEG = 24.444444444
 ENC_CLICKS_SLACK = ENC_CLICKS_PER_DEG * 3.0  # 3.0 degrees of slack
 
@@ -78,13 +81,47 @@ if __name__ == "__main__":
             # else keep turning
             else:
                 twist = gmm.Twist()
+                twist.linear.x = 0.0
                 twist.angular.z = NAV_TURN_SPEED
                 navi.publish(twist)
             continue  # TODO
             
+        elif robot_state = STATE_PREPARE_YEET:
+            # set encoder targets for 360 degree turn
+            left_encoder_target = left_encoder - (ENC_CLICKS_PER_DEG * 360.0)
+            right_encoder_target = right_encoder + (ENC_CLICKS_PER_DEG * 360.0)
+            robot_state = STATE_YEET
+            continue
+            
+        elif robot_state = STATE_YEET:
+            # Spin quickly 360 degrees to yeet enemy object
+            if abs(left_encoder - left_encoder_target) < ENC_CLICKS_SLACK or abs(
+                    right_encoder - right_encoder_target) < ENC_CLICKS_SLACK:
+                robot_state = STATE_SCAN_OBJECTS
+            else:
+                twist = gmm.Twist()
+                twist.linear.x = 0.0
+                twist.angular.z = YEET_SPEED
+                navi.publish(twist)
+            continue
+            
         elif robot_state == STATE_ALIGN_OBJECT:
             # Align so scanned object is centered with the image
+            robot_state = STATE_APPROACH_OBJECT
             continue # TODO
+            
+        elif robot_state == STATE_APPROACH_OBJECT:
+            # Go toward object until it is close enough not to be seen
+            if object_visible: # TODO
+                twist = gmm.Twist()
+                twist.linear.x = FORWARD_SPEED
+                twist.angular.z = 0.0
+                navi.publish(twist)
+                
+            # else YEET
+            else:
+                robot_state = STATE_PREPARE_YEET
+            continue
 
         elif robot_state == STATE_DIVERT_EDGE:
             # Set the encoder targets for a 180 degree turn
