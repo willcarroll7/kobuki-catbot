35e55ad9cb8423dc371e55471dc2cd4dfb39fadb
Lucas Cagle <lucas@cavs.msstate.edu>
2019-04-24 15:52:11 -0500

    fixed edge detection

--- catbot-ros.py
+++ catbot-ros.py
@@ -13,9 +13,10 @@ import geometry_msgs.msg as gmm
 
 # FSM States
 STATE_SCAN_OBJECTS = 0
-STATE_PREPARE_YEET = 95
-STATE_YEET = 96
-STATE_ALIGN_OBJECT = 97
+STATE_PREPARE_YEET = 10
+STATE_YEET = 11
+STATE_ALIGN_OBJECT = 12
+STATE_APPROACH_OBJECT = 13
 STATE_DIVERT_EDGE = 98
 STATE_TURN_180 = 99
 
@@ -23,25 +24,29 @@ STATE_TURN_180 = 99
 CLIFF_SENSORS = ["Left", "Center", "Right"]
 NAV_TURN_SPEED = math.pi / 4.0
 YEET_SPEED = math.pi
-FORWARD_SPEED = 1.0
+FORWARD_SPEED = 0.1
 ENC_CLICKS_PER_DEG = 24.444444444
 ENC_CLICKS_SLACK = ENC_CLICKS_PER_DEG * 3.0  # 3.0 degrees of slack
 
 # Global Variables
 robot_state = STATE_SCAN_OBJECTS
+cliff_sensors = [False, False, False]
 left_encoder = right_encoder = 0
+object_visible = False
 
 
-def cliff_event_callback(event: kmm.CliffEvent):
+def cliff_event_callback(event):
     """Triggered upon Turtlebot cliff detector change event.
 
     If an edge is detected, the robot enters a special edge diversion state which redirects the robot away from the edge
     and resets it into the environment scanning state."""
 
-    global robot_state
+    global cliff_sensors, robot_state
+
+    cliff_sensors[event.sensor] = event.state
 
     # Ignore undetection of cliff
-    if event.state == 0:
+    if event.state == 0 or robot_state == STATE_TURN_180:
         return
 
     print("*CLIFF DETECTED*")
@@ -52,7 +57,7 @@ def cliff_event_callback(event: kmm.CliffEvent):
     robot_state = STATE_DIVERT_EDGE
 
 
-def sensor_core_callback(msg: kmm.SensorState):
+def sensor_core_callback(msg):
     """Triggered upon Turtlebot sensor data received."""
 
     global left_encoder, right_encoder
@@ -63,18 +68,18 @@ def sensor_core_callback(msg: kmm.SensorState):
 
 # Setup application and run FSM loop
 if __name__ == "__main__":
-    rospy.init_node("catbot", anonymous=True)
+    rospy.init_node("catbot",  anonymous=True)
     rospy.Subscriber("/mobile_base/events/cliff", kmm.CliffEvent, cliff_event_callback)
     rospy.Subscriber("/mobile_base/sensors/core", kmm.SensorState, sensor_core_callback)
     navi = rospy.Publisher("/cmd_vel_mux/input/navi", gmm.Twist, queue_size=10)
-    rate = rospy.rate(10)  # 10 Hz
+    rate = rospy.Rate(10)  # 10 Hz
 
     # State helper variables
     left_encoder_target = right_encoder_target = 0
     while not rospy.is_shutdown():
         if robot_state == STATE_SCAN_OBJECTS:
             # Look for objects and spin until one is found
-            object_found = False 
+            object_found = True 
             if object_found:
                 robot_state = STATE_ALIGN_OBJECT
                 
@@ -86,14 +91,14 @@ if __name__ == "__main__":
                 navi.publish(twist)
             continue  # TODO
             
-        elif robot_state = STATE_PREPARE_YEET:
+        elif robot_state == STATE_PREPARE_YEET:
             # set encoder targets for 360 degree turn
             left_encoder_target = left_encoder - (ENC_CLICKS_PER_DEG * 360.0)
             right_encoder_target = right_encoder + (ENC_CLICKS_PER_DEG * 360.0)
             robot_state = STATE_YEET
             continue
             
-        elif robot_state = STATE_YEET:
+        elif robot_state == STATE_YEET:
             # Spin quickly 360 degrees to yeet enemy object
             if abs(left_encoder - left_encoder_target) < ENC_CLICKS_SLACK or abs(
                     right_encoder - right_encoder_target) < ENC_CLICKS_SLACK:
@@ -124,10 +129,17 @@ if __name__ == "__main__":
             continue
 
         elif robot_state == STATE_DIVERT_EDGE:
-            # Set the encoder targets for a 180 degree turn
-            left_encoder_target = left_encoder - (ENC_CLICKS_PER_DEG * 180.0)
-            right_encoder_target = right_encoder + (ENC_CLICKS_PER_DEG * 180.0)
-            robot_state = STATE_TURN_180
+            twist = gmm.Twist()
+            twist.linear.x = -FORWARD_SPEED / 2.0
+            twist.angular.z = 0.0
+            navi.publish(twist)
+            
+            if not cliff_sensors[0] and not cliff_sensors[1] and not cliff_sensors[2]:
+                # Set the encoder targets for a 180 degree turn
+                left_encoder_target = left_encoder - (ENC_CLICKS_PER_DEG * 180.0)
+                right_encoder_target = right_encoder + (ENC_CLICKS_PER_DEG * 180.0)
+                robot_state = STATE_TURN_180
+            
             continue
 
         elif robot_state == STATE_TURN_180:
@@ -144,3 +156,4 @@ if __name__ == "__main__":
             continue
 
         rate.sleep()
+
